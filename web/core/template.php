53ae4ed255c93e7d643d0ea48c75c696aaf2f04b
Florin <zero_efect@yahoo.com>
2013-04-25 12:11:00 +0300

    update

--- web/core/template.php
+++ web/core/template.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?=$page[$pag]['title']?> ~ maga</title>
-<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
 
 </head>
 <body>
