794388fc807e333cc1f116e5590d0dafd77e2a85
Florin <zero_efect@yahoo.com>
2013-04-25 11:57:00 +0300

    asa

--- web/core/template.php
+++ web/core/template.php
@@ -1,10 +1,36 @@
-<!DOCTYPE HTML>
-<html lang="ro">
-    <head>
-        <meta charset="UTF-8">
-        <title><?=$page[$pag]['title']?> ~ maga</title>
-    </head>
-    <body>
-        <?=$continut?>
-    </body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?=$page[$pag]['title']?> ~ maga</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+<body>
+<div class="body" align="center">
+
+    <div class="login" align="right">
+    	<a href="login.html"> <input type="button" value="  Admin  " /> </a>
+    </div>
+
+    <div class="meniu">
+    	<a href="index.html"> <input type="button" value="  Acasa  " /> </a>
+    	<a href="listaprod.html"> <input type="button" value="  Produse  " /> </a>
+    	<a href="cos.html"> <input type="button" value="  Cosul meu  " /> </a>
+        <a href="contact.html"> <input type="button" value="  Contact  " /> </a>
+    </div>
+
+
+    <div class="pagina" >
+         <?=$continut?>
+     </div>
+    
+    
+    <div class="footer">
+    	All rights reserved to Adi, Bianca, Diana, Florin 
+    </div>
+
+
+</div>
+</body>
 </html>
