c317201be2230d2745ffd4d9915ac17d72230698
Adrian Munteanu <munteanu92@yahoo.com>
2013-04-26 20:47:09 +0300

    Update pages.php

--- web/core/pages.php
+++ web/core/pages.php
@@ -1,13 +1,11 @@
 <?php
 
 $defaultpage='/';
-$page=array(
+$page=array
     '/'=>array('file'=>'acasa.php', 'title'=>'Acasa'),
 	'/admin'=>array('file'=>'login.php','title'=>'Log IN'),
 	'/contact'=>array('file'=>'contact.php','title'=>'Contact'),
 	'/produse'=>array('file'=>'listaprod.php','title'=>'Produse'),
 	'/cos'=>array('file'=>'cos.php','title'=>'Cosul de cumparaturi'),
 	 '/produs'=>array('file'=>'produs.php', 'title'=>'Detalii Produs'),
-
-
-?>
+);
